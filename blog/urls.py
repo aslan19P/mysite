7d609ec6fbf4bd316e959e81e3cfab1b5f5c8310
aslan <matackubovaslan@gmail.com>
2024-06-22 18:14:42 +0500

    shto

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from . import views
+from .views import post_list, post_detail
 
 app_name = 'blog'
 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('', post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, get_object_or_404
-from django.shortcuts import render
-from django.http import Http404
+from django.core.paginator import Paginator
 from .models import Post
 # Create your views here.
 
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post = Post.published.all()
+    paginator = Paginator(post, 2)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+
     context = {
         'posts': posts
     }
@@ -15,9 +18,9 @@ def post_list(request):
 
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
 
-    post = get_object_or_404(Post,id=id, status=Post.Status.PUBLISHED) 
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day) 
 
     return render(request,'blog/post/detail.html', {'post': post})
 
