66c16a54d85750d2c41c04525e0760b978375231
aslan <matackubovaslan@gmail.com>
2024-06-27 17:48:34 +0500

    qwerty

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from .forms import EmailPostForm
+# from django.views.generic import ListView
 # Create your views here.
 
+
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 2
+#     template_name = 'blog/post/list.html'
 
 
 def post_list(request):
     post = Post.published.all()
+
     paginator = Paginator(post, 2)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(1)
 
     context = {
         'posts': posts
@@ -26,4 +41,13 @@ def post_detail(request, year, month, day, post):
 
 
 
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
 
